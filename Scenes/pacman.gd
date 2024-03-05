e647102db7be4d919ad0c98c4d327770c11daed6
Kent4r <egor.bukharov06@gmail.com>
2024-03-05 13:53:02 +0300

    tf

--- Scenes/pacman.gd
+++ Scenes/pacman.gd
@@ -2,28 +2,55 @@ extends CharacterBody2D
 
 class_name Player
 
-@export var speed = 150
+#variables
+var next_movement_direction = Vector2.ZERO
 var movement_direction = Vector2.ZERO
+var shape_query = PhysicsShapeQueryParameters2D.new()
 
+#export variables
+@export var speed = 150
 
+#onready variables
+@onready var direction_pointer = $DirectionPointer
+@onready var collision_shape_2d = $CollisionShape2D
+
+func _ready():
+	shape_query.shape = collision_shape_2d.shape
+	#shape_query.collide_with_areas = false
+	#shape_query.collide_with_bodies = true
+	shape_query.collision_mask = 2
 
 func _physics_process(delta):
 	get_input()
 	
+	if movement_direction == Vector2.ZERO:
+		movement_direction = next_movement_direction
+	if can_move_in_direction(next_movement_direction, delta):
+		movement_direction = next_movement_direction
+		
 	velocity = movement_direction * speed
 	move_and_slide()
 	
 func get_input():
 	
 	if Input.is_action_pressed("left"):
-		movement_direction = Vector2.LEFT
+		next_movement_direction = Vector2.LEFT
 		rotation_degrees = 0
 	elif Input.is_action_pressed("right"):
-		movement_direction = Vector2.RIGHT
+		next_movement_direction = Vector2.RIGHT
 		rotation_degrees = 180
 	elif Input.is_action_pressed("down"):
-		movement_direction = Vector2.DOWN
+		next_movement_direction = Vector2.DOWN
 		rotation_degrees = 270
 	elif Input.is_action_pressed("up"):
-		movement_direction = Vector2.UP
+		next_movement_direction = Vector2.UP
 		rotation_degrees = 90
+	elif Input.is_action_pressed("stop"):
+		next_movement_direction = Vector2.ZERO
+
+func can_move_in_direction(dir: Vector2, delta: float) -> bool:
+	shape_query.transform = global_transform.translated(dir * speed * delta * 2)
+	var result = get_world_2d().direct_space_state.intersect_shape(shape_query)
+	return result.size() == 0
+	
+	
