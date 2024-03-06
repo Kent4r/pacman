d0b66df5795d1909decd6121d714d5e19bdc53ec
Kent4r <egor.bukharov06@gmail.com>
2024-03-06 09:18:14 +0300

    large pellet are eadable

--- pellet.gd
+++ pellet.gd
@@ -6,11 +6,11 @@ signal pellet_eaten(pallet: Pellet)
 
 @export var should_allow_eating_ghosts = false
 
+
+
 func _on_body_entered(body):
 	if body is Player:
 		pellet_eaten.emit(self)
 		queue_free()
 		if should_allow_eating_ghosts:
 			pass
-	
-		
